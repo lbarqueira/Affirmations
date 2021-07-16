f6f4d27e0d99f007fef826e4000b929519677c25
Luis Barqueira <65919567+lbarqueira@users.noreply.github.com>
2021-07-16 22:23:18 +0100

    Initial commit

---
+++ app/src/main/java/com/lbarqueira/affirmations/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lbarqueira.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/lbarqueira/affirmations/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.lbarqueira.affirmations.model
+
+data class Affirmation (val stringResourceId: Int)
